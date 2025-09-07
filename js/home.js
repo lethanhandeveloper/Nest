1fb0466380cad57d162ace6043031ce2c264e565
Lê Thành An <lethanhan19062001@gmail.com>
2025-09-07 20:23:17 +0700

    [0709]_code

--- js/home.js
+++ js/home.js
@@ -7,3 +7,38 @@ catMenu.addEventListener('mouseenter', () => {
 catMenu.addEventListener('mouseleave', () => {
   dropdown.style.display = 'none';
 });
+
+// product fly to cart
+const cart = document.querySelector(".header__nav-shortcut-item-cart");
+const addToCartBtns = document.querySelectorAll(".main__container-pditem-addcartbtn");
+
+addToCartBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const productItem = btn.closest(".main__container-product-item");
+    const productImg = productItem.querySelector(".main__container-pditem-img");
+
+    const productImgClone = productImg.cloneNode(true);
+    productImgClone.classList.add("main__container-pditem-flyimg");
+
+    document.body.appendChild(productImgClone);
+
+    const imgRect = productImg.getBoundingClientRect();
+    const cartRect = cart.getBoundingClientRect();
+
+    productImgClone.style.left = imgRect.left + "px";
+    productImgClone.style.top = imgRect.top + "px";
+    productImgClone.style.width = imgRect.width + "px";
+    productImgClone.style.height = imgRect.height + "px";
+    productImgClone.offsetWidth;
+
+    productImgClone.style.left = cartRect.left + "px";
+    productImgClone.style.top = cartRect.top + "px";
+    productImgClone.style.width = "30px";
+    productImgClone.style.height = "30px";
+    productImgClone.style.opacity = "0.3";
+
+    productImgClone.addEventListener("transitionend", () => {
+      productImgClone.remove();
+    });
+  });
+});
